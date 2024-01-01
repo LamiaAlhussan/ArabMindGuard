bdd1e7c50a1b87e4ef3234be8a7674e54c677bbf
Lamia Alhussan <104396584+LamiaAlhussan@users.noreply.github.com>
2024-01-01 19:18:57 +0300

    changes added

--- home.py
+++ home.py
@@ -1,52 +1,23 @@
-# # streamlit_app.py
-# import streamlit as st
-# import joblib
-# import pandas as pd
-# from sklearn.feature_extraction.text import TfidfVectorizer
-# from sklearn.model_selection import train_test_split
-# from sklearn.metrics import accuracy_score
-# from streamlit_extras.switch_page_button import switch_page
-# from st_pages import Page, show_pages, hide_pages
-# import tool
-
-
-# def app():
-
-#     hide_pages([
-#         Page('tool.py','Tool')
-#     ])
-
-# # Load your model and vectorizer
-#     model_filename = 'logistic_regression_model.pkl'
-#     logistic_regression_model = joblib.load(model_filename)
-
-#     # Streamlit UI
-#     st.title("Depression Detection Statistics")
-
-#     # Display accuracy
-#     st.write(f"Model Accuracy: {logistic_regression_model*100:.2f}%")
-
-#     # "Try Our Tool" button
-#     if st.button("Try Our Tool"):
-#         switch_page("Tool")
-        
-
-
-
 import streamlit as st
 import joblib
+from PIL import Image
+
 
 
 def app():
     # Load your model and vectorizer
-    model_filename = 'logistic_regression_model.pkl'
+    model_filename = 'Saudi_percentage.pkl'
     logistic_regression_model = joblib.load(model_filename)
+        
+
 
     # Streamlit UI
     st.title("Depression Detection Statistics")
 
     # Display accuracy
-    st.write(f"Model Accuracy: {logistic_regression_model*100:.2f}%")
+    st.write(f"Model Accuracy: {logistic_regression_model:.2f}%")
+    image = Image.open('dataset_wordcloud.png')
+    st.image(image, caption='',use_column_width=True)
 
     # "Try Our Tool" button
 #     if st.button("Try Our Tool"):
--- tool.py
+++ tool.py
@@ -1,12 +1,20 @@
 # tool.py
 import streamlit as st
 import joblib
+import LR_model as model
+import gather_user_tweets 
+import numpy as np
+from PIL import Image
+
+
 # import main
 
 
 # Load the logistic regression model
 model_filename = 'logistic_regression_model.pkl'
 logistic_regression_model = joblib.load(model_filename)
+best_model= joblib.load('LR_Model.pkl')
+vectorizer = joblib.load('vectorizer.pkl')
 
 def app():
     # show_pages([Page("tool.py","Tool"),
@@ -24,14 +32,18 @@ def app():
     if st.button("جرب"):
         if twitter_account:
             st.write(f"@{twitter_account} :حسابك هو ")
-
+            user_file = twitter_account+'.csv'
+            # gather_user_tweets.fetch_and_save_tweets(twitter_account,user_file)
+            Percentage = model.analyze_user_tweets("sah_khail.csv",vectorizer,best_model)
             # Predict depression probability
                 # Predict depression probability
-            probability=logistic_regression_model
-            st.write(f"{probability*100:.2f}% : احتمالية الاكتئاب ")
+            st.write(f"{Percentage:.2f}% : احتمالية الاكتئاب ")
+
+            image = Image.open('depression_wordcloud.png')
+            st.image(image, caption='3 marla plot',use_column_width=True)
 
                     # Check if the probability is higher than 60%
-            if probability >= 0.6:
+            if Percentage >= 70:
                 st.warning("تحذير : احتمالية اكتئاب عالية.")
                 st.write("هنا بعض العيادات الي ممكن تساعدك:")
                 st.markdown("[1-لبيه](https://labayh.net/ar/)")
